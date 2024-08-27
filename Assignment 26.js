6d9babd3673d54087005c7ff07282a8f16fee151
Sharon Nrangi <sharonnarangi@gmail.com>
2024-08-27 09:07:21 +0300

    Learning JS

---
+++ Assignment 26.js
@@ -0,0 +1,16 @@
+//Array Methods -- 5 examples - code
+// String Methods -- 5 examples - code
+
+//Answers
+//1. Array Methods -- 5 examples - code
+
+const items ={
+    { Name 'Bike'
+      Name 'TV'
+      Name
+      Name
+      Name
+      Name
+      Name
+      Name
+}
--- IndexMosh.js
+++ IndexMosh.js
@@ -1,1 +1,30 @@
 console.log('Hello World');
+//variables
+let name;
+console.log('Sharon');//string literal
+//Cannot be a reserved keyword
+//Should be meaningful and descriptive
+//Cannot start with a number
+//Cannot contain a space or hyphen
+//They are case sensintive
+
+//Constants
+let interestRate = 0.3
+interestRate = 1
+console.log(interestRate);
+//If I don't need to reasign Const should be my best practice
+//Let reasigns 
+
+
+//Primitive/Valuetypes
+//String
+let name ='Sharon'
+//number 
+let age = 30
+//bolean
+let isApproved = False
+//null
+let selectedColor = null
+//undefined
+
+//Dynamic Typing
