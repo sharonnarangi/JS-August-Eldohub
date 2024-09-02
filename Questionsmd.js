bb7417fbe4e0e72befa1de6eec31dbbab86a51ca
Sharon Nrangi <sharonnarangi@gmail.com>
2024-09-02 21:03:49 +0300

    QUESTIONS.MD

--- Questionsmd.js
+++ Questionsmd.js
@@ -21,3 +21,55 @@ let reggaeSongs = ['Wah gwaan','Jah is alive'];
 let sortedPlaylist = mergeArrays(gospelSongs, reggaeSongs);
 
 console.log(sortedPlaylist)
+
+
+//Question 2
+//Write a function removeDuplicates(nums)
+//that takes an array of numbers nums and removes any duplicate
+// numbers, returning a new array without duplicates.
+
+//Senario
+//You are developing a survey application where users 
+//can select multiple options. Write a function to remove duplicate
+//selections from the user's responses to ensure accurate data analysis.
+
+
+//SOLUTION
+function removeDuplicates(nums) {
+
+    let uniqueSet = new Set(nums);
+    
+    let uniqueArray = Array.from(uniqueSet);
+    
+    return uniqueArray;
+}
+let numbers = [1, 2, 2, 3, 4, 4, 5];
+let uniqueNumbers = removeDuplicates(numbers);
+console.log(uniqueNumbers);
+
+
+//Question 3
+//Write a function capitalizeWords(sentence) that takes a sentence
+//sentence and capitalizes the first letter of each word in the sentence.
+
+//Scenario
+//You are developing a blog platform where users can submit articles. 
+//Write a function to automatically capitalize the titles of submitted 
+//articles for consistent formatting and readability.
+
+
+//SOLUTION
+function capitalizeWords(sentence) {
+    let words = sentence.split(' ');
+    
+    let capitalizedWords = words.map(word => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    
+    return capitalizedWords.join(' ');
+}
+let sentence = "hello world";
+let capitalizedSentence = capitalizeWords(sentence);
+console.log(capitalizedSentence);
+
+//Question 4
