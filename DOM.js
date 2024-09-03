5e59ba7e281e3a4309b65ee614c768dd2a81f35a
Sharon Nrangi <sharonnarangi@gmail.com>
2024-09-03 19:20:24 +0300

    DOM

---
+++ DOM.js
@@ -0,0 +1,11 @@
+//Introduction to the DOM
+//It stands for Document Object Model
+//It is a programming interface for web documents
+
+//What is an interface.......{Personal question}.....A programming interface is a broad term that refers 
+// to a set of rules and tools that allows different
+//software applications or components to communicate with each other
+
+//It represents the page so that programmers can change
+//structure, content and style.
+//It is a tree-like structure, with the root node being the document itself.
